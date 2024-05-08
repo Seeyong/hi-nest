52e8107083f79591ab4e062f9b9cae506d85132d
Seeyong <seeyong987@gmail.com>
2024-05-08 17:29:49 +0900

    add update movie dto and apply it

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,6 +10,7 @@ import {
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -36,7 +37,10 @@ export class MoviesController {
   }
 
   @Put(`:id`)
-  patch(@Param('id') movieId: number, @Body() updateData): void {
+  patch(
+    @Param('id') movieId: number,
+    @Body() updateData: UpdateMovieDto,
+  ): void {
     return this.moviesService.update(movieId, updateData);
   }
 }
